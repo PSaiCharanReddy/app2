0322108cc57ff85b3b85e430ef7e27313e87167f
MrRowdy79 <psaicharanreddy7920@gmail.com>
2024-03-02 18:41:40 +0530

    my first android app

---
+++ app/src/main/java/com/microsoft/app2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.microsoft.app2
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
